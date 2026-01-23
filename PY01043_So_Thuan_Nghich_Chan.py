8113b70f1df100831db5ed707968decaaf99c280
SangPK34 <Sangphamkhac05@gmail.com>
2026-01-23 11:07:57 +0700

    AC

--- PY01043_So_Thuan_Nghich_Chan.py
+++ PY01043_So_Thuan_Nghich_Chan.py
@@ -1,18 +1,17 @@
-def check(s):
-    if len(s) % 2 ==1:
-        return False
-    r = s[::-1]
-    for i in range(0, len(r)):
-        if int(s[i]) % 2==1:
-            return False
-        if r[i] != s[i]:
-            return False
+def tn(s):
+    if len(s) %2 != 0: return False
+    z = s[::-1]
+    for i in range(len(z)):
+        if z[i] != s[i] or int(z[i]) %2 !=0: return False
     return True
+from itertools import product
 
 t = int(input())
 for _ in range(t):
     n = int(input())
-    for i in range(2, n, 2):
-        if check(str(i)):
-            print(i, end=" ")
-    print("")
+    for i in range(1, 4):
+        for p in product("02468", repeat=i):
+            s = "".join(p)+"".join(p)[::-1]
+            if s[0] != '0' and int(s)<n and tn(s):
+                print(s, end = " ")
+    print()
