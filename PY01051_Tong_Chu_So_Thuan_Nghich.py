88958e607c7a556ff4a19f81c8973b5a50ff3e50
SangPK34 <Sangphamkhac05@gmail.com>
2026-02-01 00:06:25 +0700

    AC

--- PY01051_Tong_Chu_So_Thuan_Nghich.py
+++ PY01051_Tong_Chu_So_Thuan_Nghich.py
@@ -3,8 +3,9 @@ for _ in range(t):
     s = input()
     A=0
     for i in s:
-        A+=int(i)
+        A += int(i)
     if str(A) == str(A)[::-1] and len(str(A))>1:
         print("YES")
     else :
         print("NO")
+
